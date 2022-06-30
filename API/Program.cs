e94bc3fa267b76040ae28d923a83e23cac3d0e29
Viktor <viktorkalyn1999@gmail.com>
2022-06-30 02:09:12 +0200

    Add angular to project

--- API/Program.cs
+++ API/Program.cs
@@ -3,27 +3,46 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DBContext>(options=>{
-options.UseSqlServer(connection);
+builder.Services.AddDbContext<DBContext>(options =>
+{
+
+    options.UseSqlServer(connection);
+
 });
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(
+            name: "AllowOrigin",
+            builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseCors("AllowOrigin");
 app.UseAuthorization();
 
 app.MapControllers();
